b33633ed5e00756028e3fd39e6745c06b7e59f86
vitaliyoleksyn <32169244+vitaliyoleksyn@users.noreply.github.com>
2022-09-13 10:52:44 +0300

    clean up redundant files;

--- Albelli.OrderManagement.Api/Albelli.OrderManagement.Api/Models/ProductInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Albelli.OrderManagement.Api.Models
-{
-    public class ProductInfo
-    {
-        public string ProductType { get; set; }
-        public double WidthMm { get; set; }
-    }
-}
