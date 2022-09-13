64b9cf3ecdf3fd598ed3bbb812c6fe475fb9fb5f
vitaliyoleksyn <32169244+vitaliyoleksyn@users.noreply.github.com>
2022-09-13 10:47:58 +0300

    registered services; configured pipeline;

--- Albelli.OrderManagement.Api/Albelli.OrderManagement.Api/Program.cs
+++ Albelli.OrderManagement.Api/Albelli.OrderManagement.Api/Program.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore;
+﻿using Albelli.OrderManagement.Api.Persistence;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Albelli.OrderManagement.Api
@@ -8,7 +9,22 @@ namespace Albelli.OrderManagement.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<OrdersDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- Albelli.OrderManagement.Api/Albelli.OrderManagement.Api/Startup.cs
+++ Albelli.OrderManagement.Api/Albelli.OrderManagement.Api/Startup.cs
@@ -1,24 +1,47 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Albelli.OrderManagement.Application;
+using Albelli.OrderManagement.Application.Common.Mappings;
+using Albelli.OrderManagement.Application.Interfaces;
+using Albelli.OrderManagement.Api.Persistence;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System.Reflection;
+using Albelli.OrderManagement.Api.Middleware;
 
 namespace Albelli.OrderManagement.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(config =>
+            {
+                config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+                config.AddProfile(new AssemblyMappingProfile(typeof(IOrdersDbContext).Assembly));
+            });
+            
+            services.AddApplication();
+            services.AddPersistence(Configuration);
+            
+            services.AddCors(option =>
+            {
+                option.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyHeader();
+                    policy.AllowAnyMethod();
+                    policy.AllowAnyOrigin();
+                });
+            });
+
+
             services.AddControllers();
 
             // Register the Swagger generator
@@ -26,6 +49,8 @@ namespace Albelli.OrderManagement.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Order Management API", Version = "v1" });
             });
+
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +61,7 @@ namespace Albelli.OrderManagement.Api
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCustomExceptionHandler();
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
@@ -43,6 +69,9 @@ namespace Albelli.OrderManagement.Api
             });
 
             app.UseRouting();
+            app.UseHttpsRedirection();
+            app.UseCors("AllowAll");
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
